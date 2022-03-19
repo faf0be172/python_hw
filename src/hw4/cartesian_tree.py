c7ef8896671de5804a4dfbedbe36a5ef73cacaa8
Esin Maksim <maksim.esin.2002@gmail.com>
2022-03-19 12:07:21 +0300

    fast search, repr, tests

--- src/hw4/cartesian_tree.py
+++ src/hw4/cartesian_tree.py
@@ -4,11 +4,11 @@ from src.hw4.node import Node
 class CartesianTree:
     def __init__(self, nodes: dict):
         self.__nodes = nodes
-        self.__root = self.__generate_child(sorted(nodes.items(), key=lambda x: x[0]), parent=None)
+        self.__root = self.__generate_child(sorted(nodes.items(), key=lambda x: x[0]))
 
         self.__iterator = None
 
-    def __generate_child(self, available_nodes: list, parent):
+    def __generate_child(self, available_nodes: list):
         """
         General complexity: O(n*log(n))
 
@@ -23,8 +23,8 @@ class CartesianTree:
         left_part = available_nodes[: available_nodes.index((node.key, node.priority))]
         right_part = available_nodes[available_nodes.index((node.key, node.priority)) + 1 :]
 
-        node.left_child = self.__generate_child(left_part, node)
-        node.right_child = self.__generate_child(right_part, node)
+        node.left_child = self.__generate_child(left_part)
+        node.right_child = self.__generate_child(right_part)
 
         return node
 
@@ -53,8 +53,26 @@ class CartesianTree:
         return next(self.__iterator)
 
     def __contains__(self, key):
-        iterator = iter(self)
-        for node in iterator:
-            if node.key == key:
+        """
+        Complexity: O(log(n))
+        """
+        def find_key(node: Node) -> bool:
+            if key == node.key:
                 return True
-        return False
+            if key > node.key:
+                if node.right_child:
+                    return find_key(node.right_child)
+                else:
+                    return False
+
+            if key < node.key:
+                if node.left_child:
+                    return find_key(node.left_child)
+                else:
+                    return False
+
+        return find_key(self.__root)
+
+    def __repr__(self):
+        nodes_list = list(map(str, sorted(self.__nodes.items(), key=lambda x: x[0])))
+        return "[" + ", ".join(nodes_list) + "]"

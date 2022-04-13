fed01e423fd2d947c7154b92862f3b962a2448d7
Esin Maksim <maksim.esin.2002@gmail.com>
2022-04-13 13:47:36 +0300

    types update

--- src/hw1/bash.py
+++ src/hw1/bash.py
@@ -1,74 +1,63 @@
-def head(filename, rows=10):
-    if type(filename) != str:
-        raise TypeError("Invalid filename")
-    if type(rows) != int or rows <= 0:
-        raise TypeError("Invalid rows number")
+def head(filename: str, rows: int = 10):
+    if rows < 0:
+        raise TypeError("Rows number must be non-negative")
 
-    with open(filename, "r") as file:
-        print(*[s.strip() for s in file.readlines()[:rows]], sep="\n")
-
-
-def tail(filename, rows=10):
-    if type(filename) != str:
-        raise TypeError("Invalid filename")
-    if type(rows) != int or rows <= 0:
-        raise TypeError("Invalid rows number")
-
-    with open(filename, "r") as file:
-        print(*[s.strip() for s in file.readlines()[-rows:]], sep="\n")
+    with open(filename) as file:
+        for _ in range(rows):
+            print(file.readline(), end="")
 
 
-def nl(filename, separator="  ", tabs=1):
-    if type(filename) != str:
-        raise TypeError("Invalid filename")
-    if type(separator) != str:
-        raise TypeError("Invalid separator type")
-    if type(tabs) != int or tabs < 0:
-        raise TypeError("Invalid tabs number")
+def tail(filename: str, rows: int = 10):
+    if rows < 0:
+        raise TypeError("Rows number must be non-negative")
 
-    with open(filename, "r") as file:
-        rows = file.readlines()
-        print(
-            *["\t" * tabs + str(number + 1) + separator + row.strip() for (number, row) in zip(range(len(rows)), rows)],
-            sep="\n"
-        )
+    with open(filename) as file:
+        str_count = 0
+        for _ in file:
+            str_count += 1
+
+    with open(filename) as file:
+        file_iter = iter(file)
+        for _ in range(str_count - rows):
+            next(file_iter)
+        for line in file_iter:
+            print(line, end='')
+        print()
 
 
-def wc(filename, rows=True, words=True, symbols=True):
-    if type(filename) != str:
-        raise TypeError("Invalid filename")
-    if type(rows) != bool:
-        raise TypeError("Invalid rows flag type")
-    if type(words) != bool:
-        raise TypeError("Invalid words flag type")
-    if type(symbols) != bool:
-        raise TypeError("Invalid symbols flag type")
+def nl(filename: str, separator: str = "  ", tabs: int = 1):
+    if tabs < 0:
+        raise TypeError("Tabs number must be non-negative")
 
-    with open(filename, "r") as file:
-        rows_number = len(file.readlines())
-        assert rows_number >= 0
+    with open(filename) as file:
+        str_cnt = 0
+        for line in file:
+            if line.strip():
+                print("\t" * tabs + str(str_cnt + 1) + separator + line.strip())
+                str_cnt += 1
 
-        file.seek(0, 0)
-        text = file.read()
-        words_number = len(text.replace("\n", " ").split(" "))
-        assert words_number >= 0
 
-        symbols_number = len(text)
-        assert symbols_number >= 0
+def wc(filename: str, rows: bool = True, words: bool = True, symbols: bool = True):
+    with open(filename) as file:
+        str_cnt = 0
+        words_cnt = 0
+        symbols_cnt = 0
+
+        for line in file:
+            str_cnt += 1
+            words_cnt += len(line.strip().split())
+            symbols_cnt += len(line)
 
         if symbols:
-            print("Symbols: {}".format(symbols_number))
+            print("Symbols: {}".format(symbols_cnt))
         if words:
-            print("Words: {}".format(words_number))
+            print("Words: {}".format(words_cnt))
         if rows:
-            print("Rows: {}".format(rows_number))
+            print("Rows: {}".format(str_cnt))
 
 
 if __name__ == "__main__":
     head("text.txt", 3)
-    print()
-    tail("text.txt", 3)
-    print()
+    tail("text.txt", 2)
     nl("text.txt", separator=". ")
-    print()
     wc("text.txt")
--- src/hw1/vectors.py
+++ src/hw1/vectors.py
@@ -1,54 +1,30 @@
 import math
+from numbers import Real, Complex
+from typing import Sequence
 
 
-def get_inner_product(vector_a, vector_b):
-    if type(vector_a) not in [tuple, list]:
-        raise TypeError("First vector must be list or tuple")
-    if type(vector_b) not in [tuple, list]:
-        raise TypeError("Second vector must be list or tuple")
+def get_inner_product(vector_a: Sequence[Real | Complex], vector_b: Sequence[Real | Complex]):
     if len(vector_a) != len(vector_b):
         raise IndexError("Vectors dimensions are not equal")
 
-    try:
-        product = sum([x * y for (x, y) in zip(vector_a, vector_b)])
-    except TypeError as error:
-        raise ArithmeticError("Values inside could not be multiplied") from error
-    else:
-        assert type(product) in [int, float], "Product is not a number"
-        return product
+    return sum([x * y for (x, y) in zip(vector_a, vector_b)])
 
 
-def get_norm(vector):
-    if type(vector) not in [tuple, list]:
-        raise TypeError("Vector must be list or tuple")
-    try:
-        vector_norm = sum([x**2 for x in vector]) ** 0.5
-    except TypeError as error:
-        raise ArithmeticError("Values inside could not be multiplied") from error
-    else:
-        assert type(vector_norm) in [int, float], "Norm is not a number"
-        assert vector_norm >= 0, "Norm are negative"
-        return vector_norm
+def get_norm(vector: Sequence[Real | Complex]):
+    return sum([x**2 for x in vector]) ** 0.5
 
 
-def get_angle(vector_a, vector_b):
-    if type(vector_a) not in [tuple, list]:
-        raise TypeError("First vector must be list or tuple")
-    if type(vector_b) not in [tuple, list]:
-        raise TypeError("Second vector must be list or tuple")
+def get_angle(vector_a: Sequence[Real | Complex], vector_b: Sequence[Real | Complex]):
     if len(vector_a) != len(vector_b):
         raise IndexError("Vectors dimensions are not equal")
 
-    try:
-        cosine = get_inner_product(vector_a, vector_b) / (get_norm(vector_a) * get_norm(vector_b))
-        assert -1 <= cosine <= 1, "Cosine limit exceeded"
-        angle = math.acos(cosine)
+    cosine = get_inner_product(vector_a, vector_b) / (get_norm(vector_a) * get_norm(vector_b))
+    assert -1 <= cosine <= 1, "Cosine limit exceeded"
 
-    except TypeError as error:
-        raise ArithmeticError("Values inside could not be multiplied") from error
-    else:
-        assert type(angle) in [int, float], "Angle is not a number"
-        return angle
+    angle = math.acos(cosine)
+    assert isinstance(angle, Real | Complex), "Angle is not a number"
+
+    return angle
 
 
 if __name__ == "__main__":

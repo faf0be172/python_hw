2e1a1e94e37ca1e82176b05b240b8d6ac768997d
Esin Maksim <maksim.esin.2002@gmail.com>
2022-04-29 15:10:04 +0300

    fix lint

--- tests/test_2/test_task_3.py
+++ tests/test_2/test_task_3.py
@@ -1,5 +1,4 @@
 from collections import Counter
-
 from src.test_2.task_3 import process_file
 
 
@@ -7,4 +6,4 @@ def test_statistics():
     sentences_cnt, words_dict = process_file("text.txt")
     assert sentences_cnt == 9
     assert len(words_dict) == 84
-    assert Counter(words_dict).most_common(5) == [('the', 13), ('in', 11), ('2', 11), ('of', 10), ('age', 10)]
+    assert Counter(words_dict).most_common(5) == [("the", 13), ("in", 11), ("2", 11), ("of", 10), ("age", 10)]
